96404bf071927814e1945d9bb074cee9b5ca819b
ashwinipv <ashwinipv@gmail.com>
2022-02-05 15:06:32 -0500

    19.1 activities in class

--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,33 @@
 // Create the addNumbers function here.
-
+function addNumbers(){
+    var two = 2;
+    var three = 3;
+    var five= two + three;
+    console.log(five);
+}
+addNumbers();
 // Create the stringConcat function here.
+function stringConcat(){
+    var taco = 'taco';
+    var cat = 'cat';
+    var tacoCat = taco +' '+ cat;
+
+    alert(tacoCat);
+}
+stringConcat();
+
+//Example 1 of variables being used for addition
+/*var x = 50;
+var y = 100;
+var sum = x / y;
+console.log(y);
+
+//Example 2 of variables being used to contain a string
+var burger = "Big Kahuna Burger" + " " + "is good";
+console.log(burger);
+
+//Example 3 of concatenation using variables to form a sentence
+var adj = "fat";
+var noun = "dog";
+var madlib = "The " + adj + " " + noun + " " + "jumps in the river";
+console.log(madlib);*/
--- 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
+++ 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
@@ -1,1 +1,5 @@
 // Add Event Listener Below
+$("#toogle").on("click", function(){
+    $("#dropDown").slideToggle();
+    $("img").css("transform", "rotate:180deg");
+});
