a855e7b39c5812134930562d3e109b6d4fb00f15
ashwinipv <ashwinipv@gmail.com>
2022-02-08 21:07:17 -0500

    19.2 added and completed successfully

--- 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
+++ 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
@@ -1,5 +1,5 @@
 // Add Event Listener Below
-$("#toogle").on("click", function(){
+$("#toggle").on("click", function(){
     $("#dropDown").slideToggle();
     $("img").css("transform", "rotate:180deg");
 });
